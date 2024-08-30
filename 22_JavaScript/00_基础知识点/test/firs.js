81e1adf78ceaea5bed9b11caf00b31781db52784
bmwj <3030238061@qq.com>
2024-08-30 12:52:41 +0800

    第一次提交

---
+++ 22_JavaScript/00_基础知识点/test/firs.js
@@ -0,0 +1,7 @@
+(function(){
+    var names = "白玛旺久";
+    var hello = function(){
+        alert("hello"+names);
+}
+window.first = hello;
+})();
---
+++ 22_JavaScript/00_基础知识点/test/secend.js
@@ -0,0 +1,7 @@
+(function(){
+    var names = "西藏大学";
+    var hello = function(){
+        alert("hello"+names);
+}
+window.secend = hello;
+})();
